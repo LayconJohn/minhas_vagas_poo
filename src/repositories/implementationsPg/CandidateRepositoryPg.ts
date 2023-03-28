302c07d98ef5ed93d72da5f01e1975778b82381c
layconjohn <layconjohn@gmail.com>
2023-03-28 00:20:52 -0300

    doc: create and import db postgres

--- src/repositories/implementationsPg/CandidateRepositoryPg.ts
+++ src/repositories/implementationsPg/CandidateRepositoryPg.ts
@@ -1,9 +1,9 @@
+import prisma from "../../database/pg";
 import { Candidate } from "../../models/Candidates";
 import { ICandidateRepository } from "../ICandidateRepository";
 
 export class CandidateRepositoryPg implements ICandidateRepository<number>{
-    create({ id, name, skills, trainning, experience, projects }: Candidate<number>): void {
-        throw new Error("Method not implemented.");
+    async create({ name, skills, trainning, experience, projects }: Candidate<number>): Promise<void> {
     }
     getAll(): Candidate<number>[] {
         throw new Error("Method not implemented.");
